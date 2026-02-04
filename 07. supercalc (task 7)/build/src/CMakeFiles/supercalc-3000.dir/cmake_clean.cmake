9d691f3952fbc6a6ccc4c08d2c2e64a0cb9be08f
Kivviro <114917799+Kivviro@users.noreply.github.com>
2026-02-04 22:34:41 +0700

    supercalc-3000 is now available!

---
+++ 07. supercalc (task 7)/build/src/CMakeFiles/supercalc-3000.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/supercalc-3000.dir/main.c.o"
+  "CMakeFiles/supercalc-3000.dir/main.c.o.d"
+  "supercalc-3000"
+  "supercalc-3000.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/supercalc-3000.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
