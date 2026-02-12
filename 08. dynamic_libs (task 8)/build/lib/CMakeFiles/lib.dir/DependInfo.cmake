4cb8219b77077909355917ba3ec7e4fd8b2aafdd
Kivviro <114917799+Kivviro@users.noreply.github.com>
2026-02-12 19:10:52 +0700

    task8

---
+++ 08. dynamic_libs (task 8)/build/lib/CMakeFiles/lib.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/kivvi/Documents/prog/08. task8/lib/add.c" "lib/CMakeFiles/lib.dir/add.c.o" "gcc" "lib/CMakeFiles/lib.dir/add.c.o.d"
+  "/home/kivvi/Documents/prog/08. task8/lib/div.c" "lib/CMakeFiles/lib.dir/div.c.o" "gcc" "lib/CMakeFiles/lib.dir/div.c.o.d"
+  "/home/kivvi/Documents/prog/08. task8/lib/mul.c" "lib/CMakeFiles/lib.dir/mul.c.o" "gcc" "lib/CMakeFiles/lib.dir/mul.c.o.d"
+  "/home/kivvi/Documents/prog/08. task8/lib/sub.c" "lib/CMakeFiles/lib.dir/sub.c.o" "gcc" "lib/CMakeFiles/lib.dir/sub.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
