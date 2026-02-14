2d71991b1d40b08340e0c6467320f0d8053dd41a
Kivviro <114917799+Kivviro@users.noreply.github.com>
2026-02-14 11:42:20 +0700

    fix

---
+++ 07. supercalc_task_7/lib/calc.h
@@ -0,0 +1,9 @@
+#ifndef TASK7_CALC_H
+#define TASK7_CALC_H
+
+int add(int a, int b, int *result);
+int sub(int a, int b, int *result);
+int mul(int a, int b, int *result);
+int superdiv(int a, int b, double *result);
+
+#endif
