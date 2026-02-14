fabce4e5467f4d7a6faa297d60c79ae0e129de85
Kivviro <114917799+Kivviro@users.noreply.github.com>
2026-02-14 11:39:28 +0700

    fix

---
+++ 08. dynamic_libs_task8/build/lib/CMakeFiles/lib.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lib.dir/add.c.o"
+  "CMakeFiles/lib.dir/add.c.o.d"
+  "CMakeFiles/lib.dir/div.c.o"
+  "CMakeFiles/lib.dir/div.c.o.d"
+  "CMakeFiles/lib.dir/mul.c.o"
+  "CMakeFiles/lib.dir/mul.c.o.d"
+  "CMakeFiles/lib.dir/sub.c.o"
+  "CMakeFiles/lib.dir/sub.c.o.d"
+  "liblib.pdb"
+  "liblib.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
